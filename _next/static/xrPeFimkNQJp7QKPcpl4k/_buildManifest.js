9cb03297ed904c8d52fce0f6dfa2e7393f155ac5
iobami <ayobamialadenoye@yahoo.com>
2021-04-23 13:00:02 +0100

    next js update

---
+++ _next/static/xrPeFimkNQJp7QKPcpl4k/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-29f8b50614520e420c26.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-911d4259ab304ceeb3fa.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
